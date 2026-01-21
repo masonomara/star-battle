9a3ef83d9cb95bcd274264495915cb1302958362
masonomara <mason.omara@gmail.com>
2026-01-21 18:03:48 -0500

    refactoed

--- src/sieve/rules/11-overcounting/overcounting.ts
+++ src/sieve/rules/11-overcounting/overcounting.ts
@@ -1,11 +1,63 @@
 import buildRegions from "../../helpers/regions";
 import { Board, CellState } from "../../helpers/types";
 
-export default function overcounting(board: Board, cells: CellState[][]): boolean {
-  const size = board.grid.length;
-  let changed = false;
+type Axis = "row" | "col";
 
+function processAxis(
+  axis: Axis,
+  size: number,
+  board: Board,
+  cells: CellState[][],
+  lineRegions: Map<number, Set<number>>,
+  regions: Map<number, [number, number][]>,
+): boolean {
+  for (let start = 0; start < size; start++) {
+    const lineSet = new Set<number>();
+    const regSet = new Set<number>();
+
+    for (let end = start; end < size; end++) {
+      lineSet.add(end);
+      const prev = regSet.size;
+      for (const id of lineRegions.get(end)!) regSet.add(id);
+      const added = regSet.size > prev;
+
+      if (regSet.size === lineSet.size) {
+        let valid = true;
+        for (const line of lineSet) {
+          for (let i = 0; i < size && valid; i++) {
+            const [r, c] = axis === "row" ? [line, i] : [i, line];
+            if (!regSet.has(board.grid[r][c])) valid = false;
+          }
+        }
+
+        if (valid) {
+          let changed = false;
+          for (const id of regSet) {
+            for (const [r, c] of regions.get(id)!) {
+              const lineIdx = axis === "row" ? r : c;
+              if (!lineSet.has(lineIdx) && cells[r][c] === "unknown") {
+                cells[r][c] = "marked";
+                changed = true;
+              }
+            }
+          }
+          if (changed) return true;
+        }
+      }
+
+      if (end > start && added && regSet.size > lineSet.size) break;
+    }
+  }
+  return false;
+}
+
+export default function overcounting(
+  board: Board,
+  cells: CellState[][],
+): boolean {
+  const size = board.grid.length;
   const regions = buildRegions(board.grid);
+
   const regionStars = new Map<number, number>();
   for (const [id, coords] of regions) {
     let stars = 0;
@@ -33,67 +85,8 @@ export default function overcounting(board: Board, cells: CellState[][]): boolea
     }
   }
 
-  for (let start = 0; start < size; start++) {
-    const rowSet = new Set<number>();
-    const regSet = new Set<number>();
-    for (let end = start; end < size; end++) {
-      rowSet.add(end);
-      const prev = regSet.size;
-      for (const id of rowRegions.get(end)!) regSet.add(id);
-      const added = regSet.size > prev;
+  if (processAxis("row", size, board, cells, rowRegions, regions)) return true;
+  if (processAxis("col", size, board, cells, colRegions, regions)) return true;
 
-      if (regSet.size === rowSet.size) {
-        let valid = true;
-        for (const row of rowSet) {
-          for (let c = 0; c < size && valid; c++)
-            if (!regSet.has(board.grid[row][c])) valid = false;
-        }
-        if (valid) {
-          for (const id of regSet) {
-            for (const [r, c] of regions.get(id)!) {
-              if (!rowSet.has(r) && cells[r][c] === "unknown") {
-                cells[r][c] = "marked";
-                changed = true;
-              }
-            }
-          }
-          if (changed) return true;
-        }
-      }
-      if (end > start && added && regSet.size > rowSet.size) break;
-    }
-  }
-
-  for (let start = 0; start < size; start++) {
-    const colSet = new Set<number>();
-    const regSet = new Set<number>();
-    for (let end = start; end < size; end++) {
-      colSet.add(end);
-      const prev = regSet.size;
-      for (const id of colRegions.get(end)!) regSet.add(id);
-      const added = regSet.size > prev;
-
-      if (regSet.size === colSet.size) {
-        let valid = true;
-        for (const col of colSet) {
-          for (let r = 0; r < size && valid; r++)
-            if (!regSet.has(board.grid[r][col])) valid = false;
-        }
-        if (valid) {
-          for (const id of regSet) {
-            for (const [r, c] of regions.get(id)!) {
-              if (!colSet.has(c) && cells[r][c] === "unknown") {
-                cells[r][c] = "marked";
-                changed = true;
-              }
-            }
-          }
-          if (changed) return true;
-        }
-      }
-      if (end > start && added && regSet.size > colSet.size) break;
-    }
-  }
-
-  return changed;
+  return false;
 }
