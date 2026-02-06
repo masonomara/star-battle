46a4315418363e7bbad4e6006376ba391d9d14ee
masonomara <mason.omara@gmail.com>
2026-02-06 02:00:50 -0500

    changed names

--- src/sieve/rules/index.ts
+++ src/sieve/rules/index.ts
@@ -1,22 +1,6 @@
 import { Board, CellState } from "../helpers/types";
 import { BoardAnalysis } from "../helpers/boardAnalysis";
 
-import undercountingRow from "./03-confinement-inferences/undercounting/undercountingRow";
-import undercountingColumn from "./03-confinement-inferences/undercounting/undercountingColumn";
-import overcountingRow from "./03-confinement-inferences/overcounting/overcountingRow";
-import overcountingColumn from "./03-confinement-inferences/overcounting/overcountingColumn";
-import hypotheticalRowCount from "./07-direct-hypotheticals/hypotheticalRowCount";
-import hypotheticalColumnCount from "./07-direct-hypotheticals/hypotheticalColumnCount";
-import hypotheticalRowCapacity from "./99-hypotheticalRowCapacity/hypotheticalRowCapacity";
-import hypotheticalColumnCapacity from "./99-hypotheticalColumnCapacity/hypotheticalColumnCapacity";
-import hypotheticalRegionCapacity from "./99-hypotheticalRegionCapacity/hypotheticalRegionCapacity";
-import hypotheticalOneByNBreak from "./99-hypotheticalOneByNBreak/hypotheticalOneByNBreak";
-import hypotheticalTwoByTwoBreak from "./99-hypotheticalTwoByTwoBreak/hypotheticalTwoByTwoBreak";
-import adjacentRegionCapacity from "./99-adjacentRegionCapacity/adjacentRegionCapacity";
-import reservedAreaExclusions from "./99-reservedAreaExclusions/reservedAreaExclusions";
-import adjacentLineAnalysis from "./99-adjacentLineAnalysis/adjacentLineAnalysis";
-import hypotheticalBindedPlacement from "./09-confinement-hypotheticals/hypotheticalBindedPlacement";
-import tilingAdjacencyMarks from "./05-tiling-enumeration/tilingAdjacent/tilingAdjacencyMarks";
 import starNeighbors from "./01-direct-inferences/starNeighbors/starNeighbors";
 import trivialRow from "./01-direct-inferences/trivialMarks/trivialRow";
 import trivialColumn from "./01-direct-inferences/trivialMarks/trivialColumn";
@@ -24,16 +8,39 @@ import trivialRegion from "./01-direct-inferences/trivialMarks/trivialRegion";
 import forcedRow from "./01-direct-inferences/forcedPlacements/forcedRow";
 import forcedColumn from "./01-direct-inferences/forcedPlacements/forcedColumn";
 import forcedRegion from "./01-direct-inferences/forcedPlacements/forcedRegion";
+
+import reservedAreaExclusions from "./02-tiling-inferences/reservedAreaExclusions";
+
+import undercountingRow from "./03-confinement-inferences/undercounting/undercountingRow";
+import undercountingColumn from "./03-confinement-inferences/undercounting/undercountingColumn";
+import overcountingRow from "./03-confinement-inferences/overcounting/overcountingRow";
+import overcountingColumn from "./03-confinement-inferences/overcounting/overcountingColumn";
+import consumedLineRow from "./03-confinement-inferences/consumedLines/consumedLineRow";
+import consumedLineColumn from "./03-confinement-inferences/consumedLines/consumedLineColumn";
+import consumedRegionRow from "./03-confinement-inferences/consumedRegions/consumedRegionRow";
+import consumedRegionColumn from "./03-confinement-inferences/consumedRegions/consumedRegionColumn";
+
+import adjacentLineAnalysis from "./04-direct-enumerations/adjacentLineAnalysis";
+
 import {
   tilingForcedRow,
   tilingForcedColumn,
   tilingForcedRegion,
 } from "./05-tiling-enumeration/tilingForced/tilingForced";
+import tilingAdjacencyMarks from "./05-tiling-enumeration/tilingAdjacent/tilingAdjacencyMarks";
 import tilingOverhangMarks from "./05-tiling-enumeration/tilingOverhang/tilingOverhangMarks";
-import consumedLineRow from "./03-confinement-inferences/consumedLines/consumedLineRow";
-import consumedLineColumn from "./03-confinement-inferences/consumedLines/consumedLineColumn";
-import consumedRegionRow from "./03-confinement-inferences/consumedRegions/consumedRegionRow";
-import consumedRegionColumn from "./03-confinement-inferences/consumedRegions/consumedRegionColumn";
+
+import hypotheticalRowCount from "./07-direct-hypotheticals/hypotheticalRowCount";
+import hypotheticalColumnCount from "./07-direct-hypotheticals/hypotheticalColumnCount";
+import hypotheticalRowCapacity from "./07-direct-hypotheticals/hypotheticalRowCapacity";
+import hypotheticalColumnCapacity from "./07-direct-hypotheticals/hypotheticalColumnCapacity";
+
+import hypotheticalRegionCapacity from "./08-tiling-hypotheticals/hypotheticalRegionCapacity";
+import adjacentRegionCapacity from "./08-tiling-hypotheticals/adjacentRegionCapacity";
+import hypotheticalTwoByTwoBreak from "./08-tiling-hypotheticals/hypotheticalTwoByTwoBreak";
+
+import hypotheticalOneByNBreak from "./09-confinement-hypotheticals/hypotheticalOneByNBreak";
+import hypotheticalBindedPlacement from "./09-confinement-hypotheticals/hypotheticalBindedPlacement";
 
 export type Rule = (
   board: Board,
@@ -48,6 +55,7 @@ export type RuleEntry = {
 };
 
 export const allRules: RuleEntry[] = [
+  // Level 1: Direct Inferences
   { rule: starNeighbors, level: 1, name: "Star Neighbors" },
   { rule: trivialRow, level: 1, name: "Trivial Rows" },
   { rule: trivialColumn, level: 1, name: "Trivial Columns" },
@@ -55,29 +63,37 @@ export const allRules: RuleEntry[] = [
   { rule: forcedRow, level: 1, name: "Forced Rows" },
   { rule: forcedColumn, level: 1, name: "Forced Columns" },
   { rule: forcedRegion, level: 1, name: "Forced Regions" },
-  { rule: undercountingRow, level: 2, name: "Undercounted Rows" },
-  { rule: undercountingColumn, level: 2, name: "Undercounted Columns" },
-  { rule: overcountingRow, level: 2, name: "Overcounted Rows" },
-  { rule: overcountingColumn, level: 2, name: "Overcounted Columns" },
-  { rule: consumedLineRow, level: 2, name: "Consumed Line Row" },
-  { rule: consumedLineColumn, level: 2, name: "Consumed Line Column" },
-  { rule: consumedRegionRow, level: 2, name: "Consumed Region Row" },
-  { rule: consumedRegionColumn, level: 2, name: "Consumed Region Column" },
-  { rule: tilingForcedRow, level: 3, name: "Tiling Forced Rows" },
-  { rule: tilingForcedColumn, level: 3, name: "Tiling Forced Columns" },
-  { rule: tilingForcedRegion, level: 3, name: "Tiling Forced Regions" },
-  { rule: tilingAdjacencyMarks, level: 3, name: "Tiling Adjacency Marks" },
-  { rule: tilingOverhangMarks, level: 3, name: "Tiling Overhang Marks" },
+
+  // Level 2: Tiling Inferences
+  { rule: reservedAreaExclusions, level: 2, name: "Reserved Area Exclusions" },
+
+  // Level 3: Confinement Inferences
+  { rule: undercountingRow, level: 3, name: "Undercounted Rows" },
+  { rule: undercountingColumn, level: 3, name: "Undercounted Columns" },
+  { rule: overcountingRow, level: 3, name: "Overcounted Rows" },
+  { rule: overcountingColumn, level: 3, name: "Overcounted Columns" },
+  { rule: consumedLineRow, level: 3, name: "Consumed Line Row" },
+  { rule: consumedLineColumn, level: 3, name: "Consumed Line Column" },
+  { rule: consumedRegionRow, level: 3, name: "Consumed Region Row" },
+  { rule: consumedRegionColumn, level: 3, name: "Consumed Region Column" },
+
+  // Level 4: Direct Enumerations
+  { rule: adjacentLineAnalysis, level: 4, name: "Adjacent Line Analysis" },
+
+  // Level 5: Tiling Enumerations
+  { rule: tilingForcedRow, level: 5, name: "Tiling Forced Rows" },
+  { rule: tilingForcedColumn, level: 5, name: "Tiling Forced Columns" },
+  { rule: tilingForcedRegion, level: 5, name: "Tiling Forced Regions" },
+  { rule: tilingAdjacencyMarks, level: 5, name: "Tiling Adjacency Marks" },
+  { rule: tilingOverhangMarks, level: 5, name: "Tiling Overhang Marks" },
+
+  // Level 7: Direct Hypotheticals
   { rule: hypotheticalRowCount, level: 7, name: "Hypothetical Row Count" },
   {
     rule: hypotheticalColumnCount,
     level: 7,
     name: "Hypothetical Column Count",
   },
-  { rule: adjacentRegionCapacity, level: 7, name: "Adjacent Region Capacity" },
-  { rule: reservedAreaExclusions, level: 7, name: "Reserved Area Exclusions" },
-  { rule: adjacentLineAnalysis, level: 7, name: "Adjacent Line Analysis" },
-
   {
     rule: hypotheticalRowCapacity,
     level: 7,
@@ -88,17 +104,22 @@ export const allRules: RuleEntry[] = [
     level: 7,
     name: "Hypothetical Column Capacity",
   },
+
+  // Level 8: Tiling Hypotheticals
   {
     rule: hypotheticalRegionCapacity,
-    level: 7,
+    level: 8,
     name: "Hypothetical Region Capacity",
   },
-  { rule: hypotheticalOneByNBreak, level: 7, name: "Hypothetical 1×N Break" },
+  { rule: adjacentRegionCapacity, level: 8, name: "Adjacent Region Capacity" },
   {
     rule: hypotheticalTwoByTwoBreak,
-    level: 7,
+    level: 8,
     name: "Hypothetical 2×2 Break",
   },
+
+  // Level 9: Confinement Hypotheticals
+  { rule: hypotheticalOneByNBreak, level: 9, name: "Hypothetical 1×N Break" },
   {
     rule: hypotheticalBindedPlacement,
     level: 9,
