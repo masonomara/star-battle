f733ccbf130c46788a964bab18c90792ee0e1724
masonomara <mason.omara@gmail.com>
2026-02-11 16:42:33 -0500

    Fixed stale level numbering

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,7 @@
 import { Board, CellState } from "../helpers/types";
 import { BoardAnalysis } from "../helpers/boardAnalysis";
 
-// Level 1: Direct Inferences
+// Levels 1-3: Direct Inferences
 import starNeighbors from "./01-starNeighbors/starNeighbors";
 import forcedRow from "./02-forcedPlacements/forcedRow";
 import forcedColumn from "./02-forcedPlacements/forcedColumn";
@@ -10,22 +10,18 @@ import trivialRow from "./03-trivialMarks/trivialRow";
 import trivialColumn from "./03-trivialMarks/trivialColumn";
 import trivialRegion from "./03-trivialMarks/trivialRegion";
 
-// Level 2: Tiling Enumerations
+// Level 4: Tiling Enumerations
 import tilingForcedRow from "./04-tilingEnumeration/tilingForcedRow";
 import tilingForcedColumn from "./04-tilingEnumeration/tilingForcedColumn";
 import tilingForcedRegion from "./04-tilingEnumeration/tilingForcedRegion";
 import tilingAdjacencyMarks from "./04-tilingEnumeration/tilingAdjacencyMarks";
 import tilingOverhangMarks from "./04-tilingEnumeration/tilingOverhangMarks";
 
-// Level 3: Counting Enumerations
+// Level 5: Counting Enumerations
 import countingMarkRow from "./05-countingEnumeration/countingMarkRow";
 import countingMarkColumn from "./05-countingEnumeration/countingMarkColumn";
 
-// Level 6: Tiling Counting
-import tilingCountingMarkRow from "./07-tilingCounting/tilingCountingMarkRow";
-import tilingCountingMarkColumn from "./07-tilingCounting/tilingCountingMarkColumn";
-import tilingCountingForcedRow from "./07-tilingCounting/tilingCountingForcedRow";
-import tilingCountingForcedColumn from "./07-tilingCounting/tilingCountingForcedColumn";
+// Level 6: Tiling Pairs
 import tilingPairForcedRow from "./06-tilingPairs/tilingPairForcedRow";
 import tilingPairForcedColumn from "./06-tilingPairs/tilingPairForcedColumn";
 import tilingPairAdjacencyRow from "./06-tilingPairs/tilingPairAdjacencyRow";
@@ -33,25 +29,29 @@ import tilingPairAdjacencyColumn from "./06-tilingPairs/tilingPairAdjacencyColum
 import tilingPairOverhangRow from "./06-tilingPairs/tilingPairOverhangRow";
 import tilingPairOverhangColumn from "./06-tilingPairs/tilingPairOverhangColumn";
 
-// Level 6: Group Tiling Counting
+// Level 7: Tiling Counting + Group Tiling Counting
+import tilingCountingMarkRow from "./07-tilingCounting/tilingCountingMarkRow";
+import tilingCountingMarkColumn from "./07-tilingCounting/tilingCountingMarkColumn";
+import tilingCountingForcedRow from "./07-tilingCounting/tilingCountingForcedRow";
+import tilingCountingForcedColumn from "./07-tilingCounting/tilingCountingForcedColumn";
 import groupTilingCountingMarkRow from "./07-tilingCounting/groupTilingCountingMarkRow";
 import groupTilingCountingMarkColumn from "./07-tilingCounting/groupTilingCountingMarkColumn";
 
-// Level 7: Direct Hypotheticals
+// Level 8: Direct Hypotheticals
 import hypotheticalRowCount from "./08-directHypotheticals/hypotheticalRowCount";
 import hypotheticalColumnCount from "./08-directHypotheticals/hypotheticalColumnCount";
 import hypotheticalRegionCount from "./08-directHypotheticals/hypotheticalRegionCount";
 
-// Level 8: Tiling Hypotheticals
+// Level 9: Tiling Hypotheticals
 import hypotheticalRowCapacity from "./09-tilingHypotheticals/hypotheticalRowCapacity";
 import hypotheticalColumnCapacity from "./09-tilingHypotheticals/hypotheticalColumnCapacity";
 import hypotheticalRegionCapacity from "./09-tilingHypotheticals/hypotheticalRegionCapacity";
 
-// Level 9: Counting Hypotheticals
+// Level 10: Counting Hypotheticals
 import hypotheticalCountingRow from "./10-countingHypotheticals/hypotheticalUndercountingRow";
 import hypotheticalCountingColumn from "./10-countingHypotheticals/hypotheticalUndercountingColumn";
 
-// Level 10: Propagated Hypotheticals
+// Level 11: Propagated Hypotheticals
 import propagatedRowCount from "./11-propagatedHypotheticals/propagatedRowCount";
 import propagatedColumnCount from "./11-propagatedHypotheticals/propagatedColumnCount";
 import propagatedRegionCount from "./11-propagatedHypotheticals/propagatedRegionCount";
@@ -74,7 +74,7 @@ export type RuleEntry = {
 };
 
 export const allRules: RuleEntry[] = [
-  // Level 1: Direct Inferences
+  // Levels 1-3: Direct Inferences
   { rule: starNeighbors, level: 1, name: "Star Neighbors" },
   { rule: forcedRow, level: 2, name: "Forced Rows" },
   { rule: forcedColumn, level: 2, name: "Forced Columns" },
@@ -83,25 +83,24 @@ export const allRules: RuleEntry[] = [
   { rule: trivialColumn, level: 3, name: "Trivial Columns" },
   { rule: trivialRegion, level: 3, name: "Trivial Regions" },
 
-  // Level 2: Tiling Enumerations
+  // Level 4: Tiling Enumerations
   { rule: tilingForcedRow, level: 4, name: "Tiling Forced Rows" },
   { rule: tilingForcedColumn, level: 4, name: "Tiling Forced Columns" },
   { rule: tilingForcedRegion, level: 4, name: "Tiling Forced Regions" },
   { rule: tilingAdjacencyMarks, level: 4, name: "Tiling Adjacency Marks" },
   { rule: tilingOverhangMarks, level: 4, name: "Tiling Overhang Marks" },
 
-  // Level 3: Counting Enumerations
+  // Level 5: Counting Enumerations
   { rule: countingMarkRow, level: 5, name: "Counting Mark Rows" },
   { rule: countingMarkColumn, level: 5, name: "Counting Mark Columns" },
 
- // Level 6: Tiling Pairs
+  // Level 6: Tiling Pairs
   { rule: tilingPairForcedRow, level: 6, name: "Tiling Pair Forced Rows" },
   { rule: tilingPairForcedColumn, level: 6, name: "Tiling Pair Forced Columns" },
   { rule: tilingPairAdjacencyRow, level: 6, name: "Tiling Pair Adjacency Rows" },
   { rule: tilingPairAdjacencyColumn, level: 6, name: "Tiling Pair Adjacency Columns" },
   { rule: tilingPairOverhangRow, level: 6, name: "Tiling Pair Overhang Rows" },
   { rule: tilingPairOverhangColumn, level: 6, name: "Tiling Pair Overhang Columns" },
-
 
   // Level 7: Tiling Counting
   { rule: tilingCountingMarkRow, level: 7, name: "Tiling Counting Mark Rows" },
@@ -116,16 +115,16 @@ export const allRules: RuleEntry[] = [
   { rule: hypotheticalColumnCount, level: 8, name: "Hypothetical Column Count" },
   { rule: hypotheticalRegionCount, level: 8, name: "Hypothetical Region Count" },
 
-  // Level 8: Tiling Hypotheticals
+  // Level 9: Tiling Hypotheticals
   { rule: hypotheticalRowCapacity, level: 9, name: "Hypothetical Row Capacity" },
   { rule: hypotheticalColumnCapacity, level: 9, name: "Hypothetical Column Capacity" },
   { rule: hypotheticalRegionCapacity, level: 9, name: "Hypothetical Region Capacity" },
 
-  // Level 9: Counting Hypotheticals
+  // Level 10: Counting Hypotheticals
   { rule: hypotheticalCountingRow, level: 10, name: "Hypothetical Counting Row" },
   { rule: hypotheticalCountingColumn, level: 10, name: "Hypothetical Counting Column" },
 
-  // Level 10: Propagated Hypotheticals
+  // Level 11: Propagated Hypotheticals
   { rule: propagatedRowCount, level: 11, name: "Propagated Hypothetical Row Count" },
   { rule: propagatedColumnCount, level: 11, name: "Propagated Hypothetical Column Count" },
   { rule: propagatedRegionCount, level: 11, name: "Propagated Hypothetical Region Count" },
